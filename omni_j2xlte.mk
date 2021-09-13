d5c654f7fb3e4bd3e1cfd6ba10f38b29b540791c
NullCode <ishaqahmed.dev@gmail.com>
2021-09-13 10:41:47 +0600

    Attempt at fixing MTP #1

--- omni_j2xlte.mk
+++ omni_j2xlte.mk
@@ -5,13 +5,15 @@ $(call inherit-product, build/target/product/full_base_telephony.mk)
 $(call inherit-product, build/target/product/languages_small.mk)
 
 PRODUCT_COPY_FILES += \
-    device/samsung/j2xlte/prebuilt/zImage:kernel
-
+    device/samsung/j2xlte/prebuilt/zImage:kernel \
+    device/samsung/j2xlte/recovery.fstab:recovery/root/etc/recovery.fstab 
+    
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j2xlte
 PRODUCT_NAME := omni_j2xlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-J210F
 PRODUCT_MANUFACTURER := samsung
+
 PRODUCT_RELEASE_NAME := Samsung Galaxy J2 2016
 TARGET_VENDOR := samsung
