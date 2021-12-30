fc02cf6fce0b2897f6801e16abf34070ec840606
NullCode <ishaqahmed.dev@gmail.com>
2021-12-30 21:00:33 +0600

    Add TAR maker

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
+FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
+
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
 	$(call pretty,"Target boot image: $@")
 	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
@@ -12,3 +14,5 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) 		$(recovery_ramdisk) 		$(recove
 	@echo -e ${CL_GRN}"----- Lying about SEAndroid state to Samsung bootloader ------"${CL_RST}
 	$(hide) echo -n "SEANDROIDENFORCE" >> $(INSTALLED_RECOVERYIMAGE_TARGET)
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+	$(hide) tar -C $(PRODUCT_OUT) -H ustar -c recovery.img > $(FLASH_IMAGE_TARGET)
+	@echo -e ${CL_CYN}"Made Odin flashable recovery tar: ${FLASH_IMAGE_TARGET}"${CL_RST}
